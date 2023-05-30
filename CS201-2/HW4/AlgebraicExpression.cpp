3bf887c77cd3a59d0de3c8ee68aad47c70599d97
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-05-30 16:04:36 +0300

    Added additional implementation

--- CS201-2/HW4/AlgebraicExpression.cpp
+++ CS201-2/HW4/AlgebraicExpression.cpp
@@ -5,6 +5,9 @@ using namespace std;
 bool AlgebraicExpression::isCharOperator(char ch){
     return ((ch == '+') || (ch == '-') || (ch == '*') || (ch == '/'));
 }
+bool AlgebraicExpression::isOperand(char ch){
+    return (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9');
+}
 //The given operands are assumed to exclude 0
 int AlgebraicExpression::operate(char op, int n1, int n2){
     int result;
@@ -17,8 +20,39 @@ int AlgebraicExpression::operate(char op, int n1, int n2){
     return result;
 }
 // It converts an infix expression exp to its equivalent postfix form .
+//Assumes that the given exp is a valid infix expression
 string AlgebraicExpression::infix2postfix ( const string exp ){
-
+    string postfixExp = "";
+    stack<char> newStack;
+    for(int i = 0; i < exp.size(); i++){
+        char ch = exp.at(i);
+        if(this->isOperand(ch)){
+            postfixExp += ch;
+        }
+        else if(ch == '('){
+            newStack.push(ch);
+        }
+        else if(this->isCharOperator(ch)){
+            while(!newStack.empty() && newStack.top() != '(' && this->getPrecedence(ch) <= this->getPrecedence(newStack.top())){
+                postfixExp += newStack.top();
+                newStack.pop();
+            }
+            newStack.push(ch);
+        }
+        else if(ch == ')'){
+            while(newStack.top() != '('){
+                postfixExp += newStack.top();
+                newStack.pop();
+            }
+            newStack.pop();
+        }
+    }
+    //append the remaining operators to the postfixExp;
+    while(!newStack.empty()){
+        postfixExp += newStack.top();
+        newStack.pop();
+    }
+    return postfixExp;
 }
 // It evaluates a postfix expression .
 double AlgebraicExpression::evaluatePostfix ( const string exp ){
@@ -32,14 +66,20 @@ double AlgebraicExpression::evaluatePostfix ( const string exp ){
             newStack.push(currCh);
         }
         else{
-            string s2 = (string)(newStack.pop());
-            string s1 = (string)(newStack.pop());
+            string s2 = (string)(newStack.top());
+            newStack.pop();
+            string s1 = (string)(newStack.top());
+            newStack.top();
             double operand2 = stod(s1);
             double operand1 = stod(s2);
-            double value = this->operate(ch, n1, n2);
+            double value = this->operate(ch, operand1, operand2);
             newStack.push(to_string(value));
         }
     }
-    string value = newStack.pop();
+    if(newStack.empty()){
+        return INT_MIN; // problem occured
+    }
+    string value = newStack.top();
+    newStack.pop();
     return stod(value);
 }
--- CS201-2/HW4/AlgebraicExpression.h
+++ CS201-2/HW4/AlgebraicExpression.h
@@ -6,6 +6,7 @@ class AlgebraicExpression{
 public:
     // It converts an infix expression exp to its equivalent postfix form .
     string infix2postfix ( const string exp );
+    bool isOperand(char ch);
     // It evaluates a postfix expression .
     double evaluatePostfix ( const string exp );
     int operate(char op, int n1, int n2);
