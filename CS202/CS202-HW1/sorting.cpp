8acaae0f96f1e66e4164a1f6c7258524bb4be6bc
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-14 00:46:03 +0300

    merge and mergeSort functions implemented
    Later on ensure that these functions work properly.

--- CS202/CS202-HW1/sorting.cpp
+++ CS202/CS202-HW1/sorting.cpp
@@ -21,24 +21,73 @@ void bubbleSort(int *arr, const int size, int &compCount, int &moveCount){
         pass++;
     }
 }
-void mergeSort(int *arr, const int size, int &compCount, int &moveCount){
+void mergeSort(int* arr, const int first, const int last, int &compCount, int &moveCount){
+    static int invokeCount = 0; // will help us to recall number of times this function has recursively invoked itself and
+    static int initialFirst; //we will use these variables to determine whether invokeCount must be reset to 0 for later usage of the function
+    static int initialFirst; //we will use these variables to determine whether invokeCount must be reset to 0 for later usage of the function
+    static int initialLast;
+    //we will use this function to set max array size when necessary
+    if(invokeCount == 0){
+        ::maxArraySize = last - first;
+        initialFirst = first;
+        initialLast = last;
+    }
+    invokeCount++;
     int mid = first + (last - first) / 2; //sort each half
-    int length1 = mid - first; //lengths of each subarray when original array is split in two almost equal pieces
+    /*int length1 = mid - first; //lengths of each subarray when original array is split in two almost equal pieces
     int length2 = last - (mid + 1);
-    maxArraySize = length1;
-    mergeSort(arr, length1, compCount, moveCount);
-    maxArraySize = length2;
-    mergeSort(arr, length2, compCount, moveCount);
+
+    maxArraySize = length1;*/
+    mergeSort(arr, first, mid, compCount, moveCount);
+
+    //maxArraySize = length2;
+    mergeSort(arr, mid + 1, last, length2, compCount, moveCount);
+
     merge(arr, first, mid, last);
+    if(first == initialFirst && last == initialLast){
+        invokeCount = 0;
+        //initialFirst and initialLast variables will be set to desired values after the next time function has been invoked
+    }
+
 }
 void quickSort(int *arr, const int size, int &compCount, int &moveCount){
 
 }
-void merge(int* arr, int first, int mid, int last){
+void merge(int* arr, int first, int mid, int last, int& compCount, int& moveCount){
+    int* tempArray = new int[::maxArraySize];
+
     //Initialize variables
     int first1 = first;
     int last1 = mid;
     int first2 = mid + 1;
+    int last2 = last;
 
+    int index = first1;
+    while(first1 <= last1 && first2 <= last2){
+        if(theArray[first1] <= theArray[first2]){
+            tempArray[index] = theArray[first1];
+            first1++;
+        }
+        else{
+            tempArray[index] = theArray[first2];
+            first2++;
+        }
+        index++;
+        compCount++; //increment by one since we have compared two array elements
+        moveCount++; //increment by one since we are copying the element to aux array
+    }
 
+    //Add the remaining part of exhausted array if any exists
+    while(first2 <= last2){
+        tempArray[index] = theArray[first2];
+        first2++;
+        index++;
+        moveCount++;
+    }
+    for(int i = first; i <= last; i++){
+        theArray[i] = tempArray[i];
+        moveCount++;
+    }
+    //delete tempArray for avoiding memory leak
+    delete[] tempArray;
 }
