209112bbded9e02fd74fc01715842df3f70f2023
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-14 01:03:37 +0300

    Header file has been provided

--- CS202/CS202-HW1/sorting.h
+++ CS202/CS202-HW1/sorting.h
@@ -1,1 +1,6 @@
-
+#ifndef SORTING_H
+#define SORTING_H
+    void bubbleSort(int *arr, const int size, int &compCount, int &moveCount);
+    void mergeSort(int *arr, const int size, int &compCount, int &moveCount);
+    void quickSort(int *arr, const int size, int &compCount, int &moveCount);
+#endif // SORTING_H
