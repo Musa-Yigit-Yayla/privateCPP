e97b7b22b721066ac9a53a4fc680031c31e4e733
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-14 15:24:46 +0300

    Some alternations

--- CS202/CS202-HW1/sorting.cpp
+++ CS202/CS202-HW1/sorting.cpp
@@ -16,6 +16,7 @@ int maxArraySize; //will be used when merging
 void bubbleSort(int *arr, const int size, int &compCount, int &moveCount){
     bool sorted = false; //false when swaps occur
     int pass = 1; //start pass from 1 since at most n - 1 passes may occur
+    int n = size;
     while(!sorted && (pass < n)){
         sorted = true;
         for(int index = 0; index < n; index++){
@@ -36,7 +37,6 @@ void bubbleSort(int *arr, const int size, int &compCount, int &moveCount){
 void mergeSort(int* arr, const int first, const int last, int &compCount, int &moveCount){
     static int invokeCount = 0; // will help us to recall number of times this function has recursively invoked itself and
     static int initialFirst; //we will use these variables to determine whether invokeCount must be reset to 0 for later usage of the function
-    static int initialFirst; //we will use these variables to determine whether invokeCount must be reset to 0 for later usage of the function
     static int initialLast;
     //we will use this function to set max array size when necessary
     if(invokeCount == 0){
@@ -53,7 +53,7 @@ void mergeSort(int* arr, const int first, const int last, int &compCount, int &m
     mergeSort(arr, first, mid, compCount, moveCount);
 
     //maxArraySize = length2;
-    mergeSort(arr, mid + 1, last, length2, compCount, moveCount);
+    mergeSort(arr, mid + 1, last, compCount, moveCount);
 
     merge(arr, first, mid, last);
     if(first == initialFirst && last == initialLast){
@@ -63,6 +63,27 @@ void mergeSort(int* arr, const int first, const int last, int &compCount, int &m
 
 }
 void quickSort(int *arr, const int size, int &compCount, int &moveCount){
+
+}
+void partition(int* arr, int first, int last, int& compCount, int& moveCount){
+           // initially, everything but pivot is in unknown
+   int lastS1 = first;           // index of last item in S1
+   int firstUnknown = first + 1; // index of first item in unknown
+
+	// move one item at a time until unknown region is empty
+   for (  ; firstUnknown <= last; ++firstUnknown) {
+      // Invariant: theArray[first+1..lastS1] < pivot
+      //            theArray[lastS1+1..firstUnknown-1] >= pivot
+
+      // move item from unknown to proper region
+      if (theArray[firstUnknown] < pivot) {  	// belongs to S1
+		  ++lastS1;
+    	  swap(theArray[firstUnknown], theArray[lastS1]);
+      }	// else belongs to S2
+   }
+   // place pivot in proper position and mark its location
+   swap(theArray[first], theArray[lastS1]);
+   pivotIndex = lastS1;
 
 }
 void merge(int* arr, int first, int mid, int last, int& compCount, int& moveCount){
@@ -141,6 +162,7 @@ void createAscendingArrays(int *&arr1, int *&arr2, int *&arr3, const int size){
                 currValue *= -1;
             }
         }while(currValue <= prev);
+
         arr1[i] = currValue;
         arr2[i] = currValue;
         arr3[i] = currValue;
@@ -149,5 +171,26 @@ void createAscendingArrays(int *&arr1, int *&arr2, int *&arr3, const int size){
 
 }
 void createDescendingArrays(int *&arr1, int *&arr2, int *&arr3, const int size){
+    arr1 = new int[size];
+    arr2 = new int[size];
+    arr3 = new int[size];
+
+
+    int prev = INT_MAX;
+    for(int i = 0; i < size; i++){
+        int currValue;
+        do{
+            currValue = rand() % INT_MAX;//curr value that has been generated, (curr generation)
+            int sign = rand() % 2; //if 0 the sign is negative, if 1 the sign is positive
+            if(sign == 0){
+                currValue *= -1;
+            }
+        }while(currValue >= prev);
+
+        arr1[i] = currValue;
+        arr2[i] = currValue;
+        arr3[i] = currValue;
+        prev = currValue;
+    }
 
 }
