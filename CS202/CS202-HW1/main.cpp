e31f23b01b09577cf3f140c8b463bfc7acce486b
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-15 15:46:05 +0300

    Some additions to function prototypes

--- CS202/CS202-HW1/main.cpp
+++ CS202/CS202-HW1/main.cpp
@@ -1,5 +1,15 @@
-#include <iostream>
+/*
+* Title: Sorting and Algorithm Efficiency
+* Author: Musa Yiğit Yayla
+* ID: 22003108
+* Section: 1
+* Assignment: 1
+* Description: This main file is responsible of testing algorithm efficiency and displaying correlated results.
+*/
 
+#include <iostream>
+#include "sorting.h"
+#
 using namespace std;
 
 int main()
--- CS202/CS202-HW1/sorting.h
+++ CS202/CS202-HW1/sorting.h
@@ -1,16 +1,18 @@
 /*
 * Title: Sorting and Algorithm Efficiency
-* Author: Name Surname
-* ID: 21000000
+* Author: Musa Yiğit Yayla
+* ID: 22003108
 * Section: 1
 * Assignment: 1
-* Description: description of your code
+* Description: This file has function prototypes for sorting functions and their helpers, also contains files regarding array generation and other utilities etc.
 */
 #ifndef SORTING_H
 #define SORTING_H
     void bubbleSort(int *arr, const int size, int &compCount, int &moveCount);
     void mergeSort(int *arr, const int size, int &compCount, int &moveCount);
+    void merge(int* arr, int first, int mid, int last, int& compCount, int& moveCount);
     void quickSort(int *arr, const int size, int &compCount, int &moveCount);
+    void partition(int* arr, int first, int last, int& compCount, int& moveCount, int& pivotIndex)
     void displayArray(const int *arr, const int size);
     void createRandomArrays(int *&arr1, int *&arr2, int *&arr3, const int size);
     void createAscendingArrays(int *&arr1, int *&arr2, int *&arr3, const int size);
