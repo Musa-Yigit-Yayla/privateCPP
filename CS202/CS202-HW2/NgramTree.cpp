732a04cd1dac480f0a3de6ec995df67d601b83e2
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-20 23:12:41 +0300

    Additional implementation
    Implemented addNgram and its helper function.

--- CS202/CS202-HW2/NgramTree.cpp
+++ CS202/CS202-HW2/NgramTree.cpp
@@ -2,14 +2,50 @@
 #include "BSTNode.h"
 using namespace std;
 
-NgramTree::NgramTree(){
+//Global utility function prototypes
+int stringCompare(string s1, string s2);
 
+//NgramTree functions
+NgramTree::NgramTree(){
+    //generate empty bst
 }
 NgramTree::~NgramTree(){
-
+    //delete each and every node by applying post order traversal
+    this->postorderTraverse(this->deleteGivenNode());
 }
+//Add ngram to our bst if it doesn't exist
+//If it does exist then increment its count by one
 void NgramTree::addNgram( const string& ngram ){
-
+    //If root is null, directly add it
+    if(this->root == NULL){
+        this->root = new BSTNode(ngram);
+    }
+    else{
+        //search for the node
+        this->addNgramHelper(this->root, ngram);
+    }
+}
+//Perform binary search on the given subtree and add the ngram or increment count of the given ngram by one
+//Private modifier so as to avoid futile calls made by client
+void NgramTree::addNgramHelper(BSTNode* currNode, const string& ngram){
+    if(currNode != NULL){
+        if(currNode->str == ngram){
+            currNode->counter++;
+        }
+        int comparisonValue = stringCompare(currNode->str, ngram);
+        else if(comparisonValue < 0){
+            //search right subtree
+            this->addNgramHelper(currNode->rightChild, ngram);
+        }
+        else{
+            //ngram precedes currNode's string value lexicographically
+            this->addNgramHelper(currNode->leftChild, ngram); //search left subtree
+        }
+    }
+    else{
+        //we are at the position where our ngram should be added, instantiate a new node dynamically and add it to our bst
+        currNode = new BSTNode(ngram);
+    }
 }
 int NgramTree::getTotalNgramCount() const{
 
@@ -85,5 +121,17 @@ void NgramTree::postorderHelper(BSTNode* givenNode, void (*visit(BSTNode* currNo
         visit(givenNode);
     }
 }
+//private modifier
+//will delete the given node regardless of whether it belongs to the NgramTree instance that our function is being invoked on
+//Given node is assumed to be a leaf node
+//Invoke sequentially by passing this function as a parameter to postorderTraverse during destruction
+void NgramTree::deleteGivenNode(BSTNode* currNode){
+    delete currNode;
+}
 //DON'T FORGET TO IMPLEMENT << operator given in header file
 
+//Global utility functions
+//It returns negative value if s1 precedes s2 lexicographically
+int stringCompare(string s1, string s2){
+    return strcmp(s1, s2);
+}
