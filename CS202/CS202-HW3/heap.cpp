e5adb5e0701a1d10b35145c4ea3d8d4c785916f5
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-31 15:12:05 +0300

    HW3 initial commit

---
+++ CS202/CS202-HW3/heap.cpp
@@ -0,0 +1,8 @@
+/*
+* Title: Heaps and AVL Tree
+* Author: Musa Yiğit Yayla
+* ID: 22003108
+* Section: 1
+* Assignment: 3
+* Description: description of your code
+*/
---
+++ CS202/CS202-HW3/heap.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class heap{
+private:
+
+
+public:
+    void insert(const int a);
+    int maximum();
+    int popMaximum();
+};
+
