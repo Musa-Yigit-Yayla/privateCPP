ed5cdd8a1a7c459db43d6b199fc8cec485f6a6e4
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-01 00:29:17 +0300

    Alternated implementation of heapsort

--- CS202/CS202-HW3/heapsort.cpp
+++ CS202/CS202-HW3/heapsort.cpp
@@ -14,12 +14,21 @@ using namespace std;
 //initially create a heap
 void heapSort(int arr[], int length){
     heap newHeap;
-    newHeap.buildHeap(arr, length); //create the heap from the given array
-
-    int* newArr = new int[length]; //we have to create a new array to store the result since our heap data structure removes elements one by one
-    for(int i = length - 1; i >= 0; i++){
-        int currMax = heap.popMaximum();
-        newArr[i] = currMax;
+    int* newArr = new int[length];
+    for(int i = 0; i < length; i++){
+        newArr[i] = arr[i];
     }
-    arr = newArr;
+    newHeap.buildHeap(newArr, length); //create the heap from the copied array
+
+     //we have to create a new array to store the result since our heap data structure removes elements one by one
+    int leftCounter = 0;
+    for(int i = length - 1; i > 0; i++){
+        int currMax = heap.popMaximum();
+        int temp = arr[i];
+        arr[i] = currMax;
+        arr[leftCounter++] = temp;
+    }
+    if(newArr != NULL){
+        delete[] newArr;
+    }
 }
