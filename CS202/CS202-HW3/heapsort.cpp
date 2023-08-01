baa04a6aeef7e2966438c63be69da5d04f7ccc92
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-01 15:09:50 +0300

    Adjusted heapSort and main slightly

--- CS202/CS202-HW3/heapsort.cpp
+++ CS202/CS202-HW3/heapsort.cpp
@@ -21,19 +21,20 @@ void heapSort(int arr[], int length, int& moveCount, int& compCount){
     for(int i = 0; i < length; i++){
         newArr[i] = arr[i];
     }
-    newHeap.buildHeap(newArr, length, int& moveCount, int& compCount); //create the heap from the copied array
+    newHeap.buildHeap(newArr, length, moveCount, compCount); //create the heap from the copied array
 
      //we have to create a new array to store the result since our heap data structure removes elements one by one
     int leftCounter = 0;
     for(int i = length - 1; i > 0; i++){
-        int currMax = newHeap.popMaximum();
+        int currMax = newHeap.popMaximum(moveCount, compCount);
         int temp = arr[i];
         arr[i] = currMax;
         arr[leftCounter++] = temp;
+        moveCount += 3;
     }
-    if(newArr != NULL){
+    /*if(newArr != NULL){
         delete[] newArr;
-    }
+    }*/
 }
 int main(int argc, char** argv){
     string inputFileName = argv[1];
@@ -56,12 +57,15 @@ int main(int argc, char** argv){
         int currValue = stoi(currLine);
         arr[currIndex++] = currValue;
     }
+    int moveCount = 0;
+    int compCount = 0;
     newHeap.buildHeap(arr, length);
-    heapSort(arr, length);
+    heapSort(arr, length, moveCount, compCount);
 
     //write the resulting sorted array into the provided output file
     ofstream outputFile(outputFileName);
-
+    currLine = "Move Count: " << to_string(moveCount) << ", Comparison Count: " << to_string(compCount);
+    outputFile << currLine;
     for(int i = 0; i < length; i++){
         currLine = to_string(arr[i]) + "\n";
         outputFile << currLine;
